9601ee4044771596209b05d6e28ba4ce091ef653
Asad Razvi <debugme@hotmail.com>
2017-12-06 23:30:19 +0000

    Added Some Explanatory Comments

--- source/server/index.js
+++ source/server/index.js
@@ -3,6 +3,7 @@ import spdy from 'spdy'
 import path from 'path'
 import fs from 'fs'
 
+// Tell Express to create a basic HTTP server
 const server = express()
 
 // Tell Express which directory to use to serve files from
@@ -11,28 +12,25 @@ server.use(express.static(path.join(__dirname, '..', 'client')))
 // Tell Express to redirect all requests back to the default route
 server.get('*', (request, response, next) => response.redirect('/'))
 
-const {
-  env: {
-    IP = 'https://localhost',
-    PORT = 3000
-  }
-} = process
+// Pull off environment variable values passed in to this process
+const { IP = 'https://localhost', PORT = 3000 } = process.env
 
 const callback = (error) => {
   if (error) {
     console.error(error)
     return process.exit(1)
   } else {
-    console.log(`listening on ${IP}:${PORT}`)
+    console.log(`Todo Server listening on ${IP}:${PORT}`)
   }
 }
 
-// Define credentials to support HTTPS
+// Define credentials needed to support using HTTPS on this server
 const options = {
   key: fs.readFileSync('./server.key'),
   cert: fs.readFileSync('./server.crt'),
 }
 
+// Start up an HTTP2/HTTPS server to serve the Todo application
 spdy
   .createServer(options, server)
   .listen(PORT, callback)
