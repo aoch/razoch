3de1f763d691fa4327813192e4398db747b2e985
Asad Razvi <debugme@hotmail.com>
2017-12-23 23:08:12 +0000

    add-thunk: Add Example Of Handling Async Action Using Redux Thunk

--- source/client/application.jsx
+++ source/client/application.jsx
@@ -1,24 +1,26 @@
 import React, { Fragment } from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import { devToolsEnhancer } from 'redux-devtools-extension'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 
-import reducer from './reducer'
+import Form from './form/form.container'
 import Input from './input/input.container'
 import Clock from './clock/clock.container'
+import reducer from './reducer'
 
-const state = { time: '', value: '' }
-const middleware = devToolsEnhancer()
-const store = createStore(reducer, state, middleware)
+const state = { time: '', value: '', text: '' }
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose // eslint-disable-line
+const store = createStore(reducer, state, composeEnhancers(applyMiddleware(thunk)))
 const domNode = document.querySelector('main')
 const element = (
   <Provider store={store} >
     <Fragment>
+      <Form />
       <Input />
       <Clock />
     </Fragment>
   </Provider>
 )
 
-ReactDOM.render(element, domNode)
+ReactDOM.hydrate(element, domNode)
--- source/client/reducer.js
+++ source/client/reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import { clockReducer } from './clock/clock.reducer'
 import { inputReducer } from './input/input.reducer'
+import { formReducer } from './form/form.reducer'
 
 const reducer = combineReducers({
   time: clockReducer,
-  value: inputReducer
+  value: inputReducer,
+  text: formReducer
 })
 
 export default reducer
