1f203596213669fa704380a36dd4f7c42fca0b29
Asad Razvi <asad.razvi@argos.co.uk>
2017-12-28 13:21:34 +0000

    add-redux: Add Redux Observable Example

--- source/client/reducer.js
+++ source/client/reducer.js
@@ -5,12 +5,14 @@ import inputReducer from './input/input.reducer'
 
 import formThunkReducer from './form-thunk/form.thunk.reducer'
 import formPromiseReducer from './form-promise/form.promise.reducer'
+import formObservableReducer from './form-observable/form.observable.reducer'
 
 const reducer = combineReducers({
   time: clockReducer,
   value: inputReducer,
   thunkData: formThunkReducer,
-  promiseData: formPromiseReducer
+  promiseData: formPromiseReducer,
+  observableData: formObservableReducer
 })
 
 export default reducer
