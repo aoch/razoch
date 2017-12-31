9b1045ac48ad681bd4064b85f1ffe9e6dac5f9d4
Asad Razvi <debugme@hotmail.com>
2017-12-31 13:55:49 +0000

    refactor-: Add Failure Handling To Epic

--- source/client/form-epic/form.epic.js
+++ source/client/form-epic/form.epic.js
@@ -1,4 +1,6 @@
-import { Observable } from 'rxjs'
+import {
+  Observable
+} from 'rxjs'
 
 import {
   actionTypes,
@@ -10,15 +12,20 @@ const {
 } = actionTypes
 
 const {
-  fetchEpicDataSuccess
+  fetchEpicDataSuccess,
+  fetchEpicDataFailure
 } = actionCreators
 
 function getDataEpic(action$) {
+  const onSuccess = ({ name, detail }) => fetchEpicDataSuccess(name || detail)
+  const onFailure = (error) => Observable.of(fetchEpicDataFailure(error.toString()))
+  fetchEpicDataFailure()
   return action$.ofType(FETCH_EPIC_DATA_REQUEST)
     .switchMap((action) =>
       Observable.ajax
         .getJSON(action.payload)
-        .map(({ name, detail }) => fetchEpicDataSuccess(name || detail)))
+        .map(onSuccess)
+        .catch(onFailure))
 }
 
 export default getDataEpic
