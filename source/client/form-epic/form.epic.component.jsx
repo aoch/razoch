6c206f10edfe2a6d356f2b713b1e089628c1bb40
Asad Razvi <debugme@hotmail.com>
2018-03-04 22:33:54 +0000

    scratchpa: x

--- source/client/form-epic/form.epic.component.jsx
+++ source/client/form-epic/form.epic.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Form = ({ api, message }) => (
   <div>
-    <button onClick={api}>Get data via xEpic</button>
+    <button onClick={api}>Get data via yEpic</button>
     <label>{message}</label>
   </div>
 )
