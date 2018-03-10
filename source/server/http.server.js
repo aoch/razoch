2744e7b93d2feb26fb82f8594b4c2aa852f5206d
Asad Razvi <debugme@hotmail.com>
2018-03-10 21:59:18 +0000

    ssr: Fix Saga Not Working Issue

--- source/server/http.server.js
+++ source/server/http.server.js
@@ -5,41 +5,39 @@ import httpServer from 'spdy'
 import proxyMiddleware from 'http-proxy-middleware'
 import React from 'react'
 import { renderToString } from 'react-dom/server'
+import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import Application from '../client/application/application'
 import callback from './helpers/callback'
 import { devMiddleware, hotMiddleware } from './middleware/hotReload'
 import gzipMiddleware from './middleware/gzipAsset'
-import buildStore from '../store/buildStore'
+import rootReducer from '../store/rootReducer'
 
 const server = express()
 
 const { env: { PORT, BUILD_FOLDER, NODE_ENV } } = process
-const isProduction = NODE_ENV === 'production'
 
 const cache = apicache.middleware
 server.use(cache('24 hours'))
 
-const indexPath = '/'
-const processor = (request, response) => {
-  const indexFile = `${BUILD_FOLDER}/client/index.html`
-  const initialState = {}
-  const window = undefined
-  const store = buildStore(isProduction, initialState, window)
-  store.dispatch({ type: 'SET-UP-INITIAL-STATE' })
-  const template = fs.readFileSync(indexFile, 'utf-8').toString()
-  const placeholder = '<=% preloadedApplication %>'
+const handler = (request, response) => {
+  const store = createStore(rootReducer)
   const jsx = (
     <Provider store={store}>
       <Application />
     </Provider>
   )
   const realContent = renderToString(jsx)
+  const placeholder = '<=% preloadedApplication %>'
+  const indexFile = `${BUILD_FOLDER}/client/index.html`
+  const template = fs.readFileSync(indexFile, 'utf-8').toString()
   const data = template.replace(placeholder, realContent)
   response.send(data)
 }
-server.get(indexPath, processor)
+const path = '/'
+server.get(path, handler)
 
+const isProduction = NODE_ENV === 'production'
 if (isProduction) {
   server.get('*.js', gzipMiddleware)
   server.use(express.static(`${BUILD_FOLDER}/client`))
--- source/store/buildStore.js
+++ source/store/buildStore.js
@@ -1,23 +1,23 @@
 import { createStore, compose, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import { createEpicMiddleware } from 'redux-observable'
-// import createSagaMiddleware from 'redux-saga'
+import createSagaMiddleware from 'redux-saga'
 import createDebugMiddleware from 'redux-immutable-state-invariant'
 import { identity } from 'ramda'
 import noop from 'no-op'
 
 import rootEpic from './rootEpic'
-// import rootSaga from './rootSaga'
+import rootSaga from './rootSaga'
 import rootReducer from './rootReducer'
 
 const buildStore = (
   isProduction,
   initialState = {},
-  global = { __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: noop }
+  window = { __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: noop }
 ) => {
-  const { __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: devCompose } = global
+  const { __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: devCompose } = window
   const epicMiddleware = createEpicMiddleware(rootEpic)
-  // const sagaMiddleware = createSagaMiddleware()
+  const sagaMiddleware = createSagaMiddleware()
   const debugMiddleware = (isProduction ? null : createDebugMiddleware())
   const composeEnhancers = (isProduction ? compose : devCompose)
 
@@ -25,14 +25,13 @@ const buildStore = (
     debugMiddleware,
     thunkMiddleware,
     epicMiddleware,
-    // sagaMiddleware
+    sagaMiddleware
   ].filter(identity)
 
   const appliedMiddleware = applyMiddleware(...middlewareList)
   const middleware = composeEnhancers(appliedMiddleware)
-
   const store = createStore(rootReducer, initialState, middleware)
-  // sagaMiddleware.run(rootSaga)
+  sagaMiddleware.run(rootSaga)
   return store
 }
 
