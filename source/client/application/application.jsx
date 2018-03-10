06b8a4aaf91ca730b1142bf3cc9185cad3c3b58f
Asad Razvi <debugme@hotmail.com>
2018-03-10 20:11:48 +0000

    ssr: Remove Unused SCSS

--- source/client/application/application.jsx
+++ source/client/application/application.jsx
@@ -1,11 +1,15 @@
 import React, { Fragment } from 'react'
 
+import Clock from '../clock/clock.container'
+import Input from '../input/input.container'
 import FormThunk from '../form-thunk/form.thunk.container'
 import FormEpic from '../form-epic/form.epic.container'
 import FormSaga from '../form-saga/form.saga.container'
 
 export default () => (
   <Fragment>
+    <Clock />
+    <Input />
     <FormThunk />
     <FormEpic />
     <FormSaga />
