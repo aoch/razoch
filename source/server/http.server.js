a6a7299fde2bd26ebe52fceca255c8cd674e3341
Asad Razvi <debugme@hotmail.com>
2018-03-10 10:11:09 +0000

    ssr: Initial Commit - Does Not Work Yet

--- source/server/http.server.js
+++ source/server/http.server.js
@@ -3,19 +3,25 @@ import express from 'express'
 import fs from 'fs'
 import httpServer from 'spdy'
 import proxyMiddleware from 'http-proxy-middleware'
-
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import { Provider } from 'react-redux'
+import Application from '../client/application/application'
 import callback from './helpers/callback'
 import { devMiddleware, hotMiddleware } from './middleware/hotReload'
 import gzipMiddleware from './middleware/gzipAsset'
+import buildStore from '../store/buildStore'
+import logger from './helpers/logger'
 
 const server = express()
 
 const { env: { PORT, BUILD_FOLDER, NODE_ENV } } = process
+const isProduction = NODE_ENV === 'production'
 
 const cache = apicache.middleware
 server.use(cache('24 hours'))
 
-if (NODE_ENV === 'production') {
+if (isProduction) {
   server.get('*.js', gzipMiddleware)
   server.use(express.static(`${BUILD_FOLDER}/client`))
 } else {
@@ -28,8 +34,28 @@ const proxyData = { target: 'http://localhost:3001' }
 server.use(proxyPath, proxyMiddleware(proxyData))
 
 const indexPath = '*'
-const indexFile = `${BUILD_FOLDER}/client/index.html`
-const processor = (request, response) => response.sendFile(indexFile)
+const processor = (request, response) => {
+  const indexFile = `${BUILD_FOLDER}/client/index.html`
+
+  const initialState = {}
+  const window = undefined
+  const store = buildStore(isProduction, initialState, window)
+  store.dispatch({ type: 'FETCH_SAGA_DATA_REQUEST' })
+  store.dispatch({ type: 'FETCH_EPIC_DATA_REQUEST' })
+  store.dispatch({ type: 'FETCH_THUNK_DATA_REQUEST' })
+  const template = fs.readFileSync(indexFile, 'utf-8').toString()
+  const placeholder = '<=% preloadedApplication %>'
+  const jsx = (
+    <Provider store={store}>
+      <Application />
+    </Provider>
+  )
+  const realContent = renderToString(jsx)
+  const data = template.replace(placeholder, realContent)
+  logger.debug(`[xxxx] state is ${JSON.stringify(store.getState(), null, 4)}`)
+  logger.debug(`[xxxx] data is  ${JSON.stringify(data)}`)
+  response.send(data)
+}
 server.get(indexPath, processor)
 
 const options = {
