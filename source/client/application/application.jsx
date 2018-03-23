2c9bbcb0279e9795d7f0bd2c4959ec10b389f7ac
Asad Razvi <debugme@hotmail.com>
2018-03-23 21:21:10 +0000

    update-ro: Add SSR For Thunk

--- source/client/application/application.jsx
+++ source/client/application/application.jsx
@@ -1,17 +1,17 @@
 import React, { Fragment } from 'react'
 
-// import ClockContainer from '../clock/clock.container'
-// import InputContainer from '../input/input.container'
+import ClockContainer from '../clock/clock.container'
+import InputContainer from '../input/input.container'
 import FormThunkContainer from '../form-thunk/form.thunk.container'
-// import FormEpicContainer from '../form-epic/form.epic.container'
-// import FormSagaContainer from '../form-saga/form.saga.container'
+import FormEpicContainer from '../form-epic/form.epic.container'
+import FormSagaContainer from '../form-saga/form.saga.container'
 
 export default () => (
   <Fragment>
-    {/* <ClockContainer />
-    <InputContainer /> */}
+    <ClockContainer />
+    <InputContainer />
     <FormThunkContainer />
-    {/* <FormEpicContainer />
-    <FormSagaContainer /> */}
+    <FormEpicContainer />
+    <FormSagaContainer />
   </Fragment>
 )
--- source/routes/Routes.jsx
+++ source/routes/Routes.jsx
@@ -1,16 +1,8 @@
 import Application from '../client/application/application'
-import ClockContainer, { loadData as loadClockData } from '../client/clock/clock.container'
-import InputContainer, { loadData as loadInputData } from '../client/input/input.container'
-import EpicContainer, { loadData as loadEpicData } from '../client/form-epic/form.epic.container'
-import SagaContainer, { loadData as loadSagaData } from '../client/form-saga/form.saga.container'
 import ThunkContainer, { loadData as loadThunkData } from '../client/form-thunk/form.thunk.container'
 
 const Routes = [
   { path: '/', exact: true, component: Application },
-  { path: '/clock', component: ClockContainer, loadData: loadClockData },
-  { path: '/input', component: InputContainer, loadData: loadInputData },
-  { path: '/epic', component: EpicContainer, loadData: loadEpicData },
-  { path: '/saga', component: SagaContainer, loadData: loadSagaData },
   { path: '/thunk', component: ThunkContainer, loadData: loadThunkData },
 ]
 
