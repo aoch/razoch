7c6975af2731402d61dc0bfb675e70f4da88c425
Asad Razvi <debugme@hotmail.com>
2018-03-27 23:51:57 +0100

    tidy-up-c: Tidy Up Container Code

--- source/client/form-epic/form.epic.container.jsx
+++ source/client/form-epic/form.epic.container.jsx
@@ -10,12 +10,7 @@ import {
 
 const mapStateToProps = (state) => {
   const { formEpic: { done, pass, fail } } = state
-  const message = (done && pass)
-    ? pass
-    : (done && fail)
-      ? fail
-      : 'loading...'
-
+  const message = done ? (fail || pass) : 'loading...'
   const props = { message }
   return props
 }
